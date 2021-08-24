e30f6ae8b88987f2beecdbd9707190f8cc488a42
Gabe Dragland <crazygtboyz@gmail.com>
2021-08-24 11:34:18 -0600

    data_random.py changed

--- data_random.py
+++ data_random.py
@@ -8,13 +8,13 @@ total_2 = 1000
 
 fieldnames = ["x_value", "total_1", "total_2"]
 
-with open('data.csv', 'w') as csv_file:
+with open('data_random.csv', 'w') as csv_file:
     csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     csv_writer.writeheader()
 
 while True:
 
-   with open('data.csv', 'a') as csv_file:
+   with open('data_random.csv', 'a') as csv_file:
        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
        info = {
@@ -28,6 +28,6 @@ while True:
 
        x_value += 1
        total_1 = total_1 + random.randint(-6, 8)
-       total_2 = total_2 + random.randint(-5, 6)
+       total_2 = total_2 + random.randint(-6, 8)
 
    time.sleep(1)
